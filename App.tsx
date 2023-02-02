ad098edcc0aa1af143f4ade6dd04fe5e1fd1d7cc
Murilo Morandi <murilomorandialexandre2011@gmail.com>
2023-02-01 23:34:19 -0300

    Custumizado o tema padrão

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+
+import { THEME } from './src/theme';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
-      
+    <NativeBaseProvider theme={THEME}>
+
       <StatusBar
         barStyle={"light-content"}
         backgroundColor={"transparent"}
         translucent
-      />
+        />
+
+      {fontsLoaded? <View/> : <Loading/>}
       
-      {fontsLoaded ? <Text>Hello World!</Text> : <View/>}
-    </View>
+      
+    </NativeBaseProvider>
   );
 }
 
